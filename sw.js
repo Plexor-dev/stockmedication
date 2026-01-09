d5ad17a72f3851c9a3af5f3cdd2c2d83bff36371
Plexor-dev <74623252+Plexor-dev@users.noreply.github.com>
2026-01-08 22:17:28 -0300

    add more styles sesion and sw(first)

---
+++ sw.js
@@ -0,0 +1,18 @@
+// sw.js - Service Worker Básico
+const CACHE_NAME = 'stock-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
